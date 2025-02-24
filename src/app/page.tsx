334a87bf8013f07057d40ee34e35ba1b819a03ad
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-24 18:51:15 -0300

    added biome.json and some settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,3 @@
-import { ArrowRight, Copy, Mail } from "lucide-react";
-import { Button } from "@/components/button";
-import { IconButton } from "@/components/icon-button";
-import { InputField, InputIcon, InputRoot } from "@/components/input";
-
 export default function Home() {
-  return (
-    <main>
-      <Button type="button">
-        Send
-        <ArrowRight />
-      </Button>
-
-      <IconButton>
-        <Copy />
-      </IconButton>
-
-      <div>
-        <InputRoot>
-          <InputIcon>
-            <Mail className="size-5" />
-          </InputIcon>
-
-          <InputField />
-        </InputRoot>
-      </div>
-    </main>
-  );
+  return <main></main>
 }
