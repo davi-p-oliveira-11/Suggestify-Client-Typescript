75a57c5175861c1a87c6792e99006a066b52ffc9
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-02-24 16:29:12 -0300

    added css colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/button";
 export default function Home() {
   return (
     <main>
-      <div className="text-white">Hello world!</div>
+      <div className="text-white ">Hello world!</div>
 
       <Button text="John" />
       <Button text="Mary" />
