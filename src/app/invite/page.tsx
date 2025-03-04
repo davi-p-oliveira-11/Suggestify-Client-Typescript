c4ee891d51922a11b17e2ea441242be552064e89
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-04 17:56:54 -0300

    addeed some content to the invite page and installed tailwind-merge

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,3 +1,51 @@
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { Copy, Icon, Link } from 'lucide-react'
+import Image from 'next/image'
+import logo from '../../assets/logo.svg'
+
 export default function InvitePage() {
-  return <h1>Contato</h1>
+  return (
+    <div className="min-h-dvh flex items-center justify-between gap-16 md:flex">
+      <div className="flex flex-col gap-10 w-full max-w-[550px]">
+        <Image src={logo} alt="Suggestify" width={108.5} height={30} />
+
+        <div className="space-y-2">
+          <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
+            Subscription confirmed !
+          </h1>
+          <p className="text-gray-300">
+            To enter the event, access the link sent to your email.
+          </p>
+        </div>
+
+        <div className="space-y-6">
+          <div className="space-y-3">
+            <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+              Invite and earn rewards.
+            </h2>
+            <p className="text-gray-300">
+              Invite more people to the event and enter to win exclusive prizes!
+              Just share the link!
+            </p>
+          </div>
+
+          <InputRoot>
+            <InputIcon>
+              <Link className="size-5" />
+            </InputIcon>
+
+            <InputField
+              readOnly
+              defaultValue="http://localhost:3000/invite/122324352342334"
+            />
+
+            <IconButton>
+              <Copy className="size-5" />
+            </IconButton>
+          </InputRoot>
+        </div>
+      </div>
+    </div>
+  )
 }
