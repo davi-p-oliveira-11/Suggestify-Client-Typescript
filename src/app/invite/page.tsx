028521f33da6bf09de1adbe49f42ebfc256bd606
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-04 19:51:54 -0300

    finished ivite page layout

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -9,7 +9,11 @@ import {
   MousePointerClick,
 } from 'lucide-react'
 import Image from 'next/image'
+
 import logo from '../../assets/logo.svg'
+import cooper from '../../assets/medal-cooper.svg'
+import gold from '../../assets/medal-gold.svg'
+import silver from '../../assets/medal-silver.svg'
 
 export default function InvitePage() {
   return (
@@ -85,6 +89,46 @@ export default function InvitePage() {
 
               <Medal className="size-5 text-purple absolute top-3 left-3" />
             </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full max-w-[440px] space-y-5">
+        <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+          Referral Ranking
+        </h2>
+
+        <div className="space-y-4">
+          <div className="relative rounded-xl bg-gray-700 border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">1st</span> | John Connor
+            </span>
+
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+
+            <Image src={gold} alt="" className="absolute top-0 right-8" />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">2nd</span> | John Connor
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image src={silver} alt="" className="absolute top-0 right-8" />
+          </div>
+
+          <div className="relative rounded-xl bg-gray-700 border-gray-600 p-6 flex flex-col justify-center gap-3">
+            <span className="text-sm text-gray-300 leading-none">
+              <span className="font-semibold">3rd</span> | John Connor
+            </span>
+            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
+              1030
+            </span>
+            <Image src={cooper} alt="" className="absolute top-0 right-8" />
           </div>
         </div>
       </div>
